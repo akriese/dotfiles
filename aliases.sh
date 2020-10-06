dc73c20977fe272094a789bae53a730a7e0f5b53
Anton Kriese <anton.kriese@gmx.de>
2020-10-06 15:37:00 +0200

    aliases: group ssh commands; small convenient python calculator

--- aliases.sh
+++ aliases.sh
@@ -24,12 +24,16 @@ alias hsghci="cd $CODING/Haskell && ghci"
 alias vim='vim -S ~/.vimrc'
 alias vi='vim'
 alias temp='vcgencmd measure_temp'
-alias pissh='ssh -X pi@192.168.178.152'
-alias lncrna="ssh -c aes128-gcm@openssh.com -XCY -J kriea97@andorra.imp.fu-berlin.de kriea97@lncrna.imp.fu-berlin.de"
 alias rotate='xrandr -o'
 alias pdf='evince'
 alias img='eog'
 alias vim_rc='vim $CODING/dotfiles/.vimrc'
 alias bash_rc='vim $HOME/.bashrc'
+alias pissh='ssh -X pi@192.168.178.152'
+alias lncrna="ssh -c aes128-gcm@openssh.com -XCY -J kriea97@andorra.imp.fu-berlin.de kriea97@lncrna.imp.fu-berlin.de"
 alias jena='ssh -XY -J biprak6@login.fmi.uni-jena.de biprak6@zwoa.bioinf.uni-jena.de'
+
+pc () {
+  python -c "print($1)"
+}
 
