72cc9a27756cd2a9136710e2b885a7d5c1625a42
Anton Kriese <anton.kriese@gmx.de>
2020-10-25 10:31:10 +0100

    wsl fzf; pi ssh

--- aliases.sh
+++ aliases.sh
@@ -29,7 +29,7 @@ alias pdf='evince'
 alias img='eog'
 alias vim_rc='vim $CODING/dotfiles/.vimrc'
 alias bash_rc='vim $HOME/.bashrc'
-alias pissh='ssh -X pi@192.168.178.152'
+alias pissh='ssh pi'
 alias lncrna="ssh -c aes128-gcm@openssh.com -XCY -J kriea97@andorra.imp.fu-berlin.de kriea97@lncrna.imp.fu-berlin.de"
 alias jena='ssh -XY -J biprak6@login.fmi.uni-jena.de biprak6@zwoa.bioinf.uni-jena.de'
 
