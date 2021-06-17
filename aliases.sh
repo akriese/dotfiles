3aab5442aaeca6d3e8af306cdbf6d5e63d1e4667
Anton Kriese <anton.kriese@gmx.de>
2021-06-17 13:29:38 +0200

    cleanup ssh aliases

--- aliases.sh
+++ aliases.sh
@@ -28,9 +28,6 @@ alias pdf='evince'
 alias img='eog'
 alias vim_rc='vim $CODING/dotfiles/.vimrc'
 alias bash_rc='vim $HOME/.bashrc'
-alias pissh='ssh pi'
-alias lncrna="ssh -c aes128-gcm@openssh.com -XCY -J kriea97@andorra.imp.fu-berlin.de kriea97@lncrna.imp.fu-berlin.de"
-alias jena='ssh -XY -J biprak6@login.fmi.uni-jena.de biprak6@zwoa.bioinf.uni-jena.de'
 alias pubip='curl https://ipinfo.io/ip'
 alias lesss='less -S'
 
