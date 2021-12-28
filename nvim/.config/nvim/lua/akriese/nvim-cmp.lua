b6adb2cde1b0d09370870c8a13765ddeffba0672
Anton Kriese <anton.kriese@gmx.de>
2021-12-28 18:54:53 +0100

    TAB completion for nvim-cmp

--- nvim/.config/nvim/lua/akriese/nvim-cmp.lua
+++ nvim/.config/nvim/lua/akriese/nvim-cmp.lua
@@ -21,6 +21,20 @@
         c = cmp.mapping.close(),
       }),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<TAB>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+      ['<S-TAB>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
