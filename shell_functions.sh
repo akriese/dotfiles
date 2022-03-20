e7d00e8dc8b64c4eaf473e0af9513b599fbf947e
Anton Kriese <kriese_a@molgen.mpg.de>
2022-03-20 23:18:16 +0100

    remove commented out code

--- shell_functions.sh
+++ shell_functions.sh
@@ -10,9 +10,6 @@ add_to_path() {
     stripped_path=${PATH}
     new_path="$(readlink -e "${1}")"
     case "${PATH}" in
-        # *:${new_path}:*)
-        #     stripped_path="$(sed "s_:${new_path}__g" <<< "${stripped_path}")"
-        #     ;&
         *:${new_path})
             stripped_path="$(sed "s#:${new_path}##g" <<< "${stripped_path}")"
             ;&
