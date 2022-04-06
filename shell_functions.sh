2fbcf06ad5be7be0af0d5a0f1ae9091b2f310829
Anton Kriese <anton.kriese@gmx.de>
2022-04-06 13:50:45 +0200

    rename function for adding PATH, as problems occured with add_to_path and virtual environments

--- shell_functions.sh
+++ shell_functions.sh
@@ -6,7 +6,7 @@ tmux_update_display() {
     export DISPLAY="$(tmux show-env | grep ^DISPLAY | sed -n 's/^DISPLAY=//p')"
 }
 
-add_to_path() {
+put_first_in_path() {
     stripped_path=${PATH}
     new_path="$(readlink -e "${1}")"
     case "${PATH}" in
@@ -19,5 +19,9 @@ add_to_path() {
     esac
 
     export PATH="${new_path}:${stripped_path}"
+}
+
+add_to_path() {
+    [[ "$PATH" == *$1* ]] || export PATH="${1}:${PATH}"
 }
 
