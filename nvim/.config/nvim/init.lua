5dce2969ee2b8f710409c8d7f1774d477df18056
Anton Kriese <kriese_a@molgen.mpg.de>
2022-08-10 15:03:19 +0200

    translate nvim config from viml to lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,18 @@
+-- some functions for general use
+require("akriese.functions")
+-- load most of the general config (keymaps, globals, Plug, etc)
+require("akriese.nvimrc")
+
+-- Plugin setups
+require("akriese.nvim-cmp")
+require("akriese.treesitter")
+require("akriese.nvim-tree")
+require("akriese.indent")
+require("akriese.lspconfig")
+require("akriese.telescope")
+require("akriese.symbols")
+require("akriese.comment")
+require("akriese.better-escape")
+require("akriese.filetype")
+require('neoscroll').setup()
+require("akriese.dap")
