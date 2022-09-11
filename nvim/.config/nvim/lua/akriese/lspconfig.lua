7d7c5e698006e87de84af1d496b988df4d4a1624
Anton Kriese <kriese_a@molgen.mpg.de>
2022-09-11 23:16:22 +0200

    fix sumneko lsp setup

--- nvim/.config/nvim/lua/akriese/lspconfig.lua
+++ nvim/.config/nvim/lua/akriese/lspconfig.lua
@@ -58,8 +58,18 @@ lsp_installer.on_server_ready(
         end
 
         if server.name == "sumneko_lua" then
-            local luadev = require("lua-dev").setup()
-            server:setup(luadev, opts)
+            opts = vim.tbl_deep_extend("force", require("lua-dev").setup({
+                library = {
+                    vimruntime = true, -- runtime path
+                    types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+                    plugins = true, -- installed opt or start plugins in packpath
+                    -- you can also specify the list of plugins to make available as a workspace library
+                    -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
+                },
+                runtime_path = true, -- enable this to get completion in require strings. Slow!
+                -- pass any additional options that will be merged in the final lsp config
+            }), opts)
+            server:setup(opts)
         else
             server:setup(opts)
         end
