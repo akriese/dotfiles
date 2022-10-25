d7818aeca84f105596ca564bbe5bbfdbec0aeae3
Anton Kriese <anton.kriese@gmx.de>
2022-10-25 17:48:06 +0200

    update cmp_nvim_lsp config to API changes

--- nvim/.config/nvim/lua/akriese/lspconfig.lua
+++ nvim/.config/nvim/lua/akriese/lspconfig.lua
@@ -49,7 +49,7 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
