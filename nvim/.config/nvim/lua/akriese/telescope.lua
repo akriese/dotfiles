2b4450ee39d0954e4ebd7a858a48a0f0a5337aaa
Anton Kriese <anton.kriese@gmx.de>
2022-12-17 16:15:16 +0100

    diff and rebase mappings for git_commits picker

--- nvim/.config/nvim/lua/akriese/telescope.lua
+++ nvim/.config/nvim/lua/akriese/telescope.lua
@@ -2,6 +2,45 @@ local F = require("akriese.functions")
 local map = F.map
 
 local telescope = require('telescope')
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+-- rebase interactive, <C-r> already taken by reset default
+local git_rebase_interactive = function(prompt_bufnr)
+    local selection = action_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    vim.cmd("Git rebase --interactive " .. selection.value)
+end
+
+-- view diff since selected commit
+local git_diff_since = function(prompt_bufnr)
+    local selection = action_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    vim.cmd("Git diff " .. selection.value)
+end
+
+-- view diff of selected commit
+local git_diff_single = function(prompt_bufnr)
+    local selection = action_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    vim.cmd("Git show " .. selection.value)
+end
+
+-- view diff between two selected commits
+-- the diff is from the first selected to the second
+-- exactly two commits have to be selected to perform the diff
+local git_diff_between = function(prompt_bufnr)
+    local current_picker = action_state.get_current_picker(prompt_bufnr)
+    local selection = current_picker:get_multi_selection()
+    if #selection ~= 2 then
+        print('Select exactly two commits, please!')
+        return
+    end
+    local first, second = selection[1], selection[2]
+    actions.close(prompt_bufnr)
+    vim.cmd("Git diff " .. first.value .. ".." .. second.value)
+end
+
 telescope.setup {
     defaults = {
         i = {
@@ -31,6 +70,18 @@ telescope.setup {
             override_file_sorter = true, -- override the file sorter
             case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
+        }
+    },
+    pickers = {
+        git_commits = {
+            mappings = {
+                i = {
+                    ["<C-b>i"] = git_rebase_interactive,
+                    ["<C-d>s"] = git_diff_since,
+                    ["<C-d>o"] = git_diff_single,
+                    ["<C-d>b"] = git_diff_between
+                },
+            }
         }
     }
 }
