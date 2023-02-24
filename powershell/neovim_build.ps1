8f2345598ddfb88c8e64f47fd255167b6c68d4f9
Anton Kriese <anton.kriese@gmail.com>
2023-02-24 18:33:20 +0100

    extract cmake_c_compiler from commands
    cmake was not recognizing the flag somehow

--- powershell/neovim_build.ps1
+++ powershell/neovim_build.ps1
@@ -9,6 +9,7 @@
 $neovim_dir = $pwd
 
 $env:DEPS_BUILD_DIR = "$neovim_dir\.deps\"
+$env:CMAKE_C_COMPILER = "clang"
 $env:LUAJIT_LIBRARY = "${env:DEPS_BUILD_DIR}usr\lib\luajit.lib"
 $install_dir = "C:\tools\nvim_build\"
 # echo $env:LUAJIT_LIBRARY $env:CMAKE_INSTALL_PREFIX
@@ -22,13 +23,13 @@ vsdevcmd.bat -arch=x64 -no_logo
 cmake -S cmake.deps -B "$env:DEPS_BUILD_DIR" -G Ninja
 
 # build dependencies
-cmake --build "$env:DEPS_BUILD_DIR" -DCMAKE_C_COMPILER=clang
+cmake --build "$env:DEPS_BUILD_DIR"
 
 # prepare build dir; for some reason, the luajit.lib path has to be explicitly passed...
-cmake -DCMAKE_INSTALL_PREFIX="$install_dir" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_C_FLAGS=-w -B $build_dir
+cmake -DCMAKE_INSTALL_PREFIX="$install_dir" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-w -B $build_dir
 
 # build
-cmake --build $build_dir -DCMAKE_C_COMPILER=clang
+cmake --build $build_dir
 
 # install the build
 cmake --install $build_dir
