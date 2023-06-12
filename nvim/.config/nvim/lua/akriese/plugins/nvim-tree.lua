304e81e19855604a2d5b3650512acaf2a04f6433
Anton Kriese <anton.kriese@gmail.com>
2023-06-12 11:44:20 -0400

    new nvim-tree config with on_attach
    also, format with stylua :)

--- nvim/.config/nvim/lua/akriese/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/akriese/plugins/nvim-tree.lua
@@ -1,61 +1,68 @@
-require 'nvim-tree'.setup {
-    disable_netrw       = true,
-    hijack_netrw        = true,
-    open_on_setup       = false,
-    ignore_ft_on_setup  = {},
-    open_on_tab         = false,
-    hijack_cursor       = false,
-    update_cwd          = false,
-    respect_buf_cwd     = true,
-    sync_root_with_cwd  = true,
-    hijack_directories  = {
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local function on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+    vim.keymap.set("n", "+", api.tree.change_root_to_node, opts("CD"))
+end
+
+require("nvim-tree").setup({
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_tab = false,
+    hijack_cursor = false,
+    update_cwd = false,
+    respect_buf_cwd = true,
+    sync_root_with_cwd = true,
+    hijack_directories = {
         enable = true,
-        auto_open = true
+        auto_open = true,
     },
-    diagnostics         = {
+    diagnostics = {
         enable = false,
         icons = {
             hint = "",
             info = "",
             warning = "",
             error = "",
-        }
+        },
     },
     update_focused_file = {
-        enable      = true,
-        update_cwd  = true,
-        ignore_list = { "octo", "git", "fugitive*", "help" }
+        enable = true,
+        update_cwd = true,
+        ignore_list = { "octo", "git", "fugitive*", "help" },
     },
-    system_open         = {
-        cmd  = nil,
-        args = {}
+    system_open = {
+        cmd = nil,
+        args = {},
     },
-    filters             = {
+    filters = {
         dotfiles = false,
-        custom = {}
+        custom = {},
     },
-    git                 = {
+    git = {
         enable = true,
         ignore = true,
         timeout = 500,
     },
-    view                = {
+    view = {
         width = 30,
         hide_root_folder = false,
-        side = 'left',
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = "+", action = "cd" },
-            },
-        },
+        side = "left",
         number = false,
         relativenumber = false,
         signcolumn = "yes",
-        preserve_window_proportions = true
+        preserve_window_proportions = true,
     },
-    trash               = {
+    trash = {
         cmd = "trash",
-        require_confirm = true
-    }
-}
+        require_confirm = true,
+    },
+    on_attach = on_attach,
+})
