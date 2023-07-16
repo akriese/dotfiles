eb5ce9c1ec59cc75aed40a31cad8bba3e53bc3dc
Anton Kriese <anton.kriese@gmx.de>
2023-07-16 13:47:17 +0200

    fix: treesitter indentation for dart is slow af

--- nvim/.config/nvim/lua/akriese/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/akriese/plugins/treesitter.lua
@@ -42,7 +42,7 @@ require("nvim-treesitter.configs").setup({
     },
     indent = {
         enable = true,
-        disable = { "python" },
+        disable = { "python", "dart" },
     },
     rainbow = {
         enable = true,
