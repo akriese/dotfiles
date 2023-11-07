4e618a68e16b6ed3cca82056d30df755ea53f493
Anton Kriese <anton.kriese@gmail.com>
2023-11-07 10:39:28 +0100

    chore(indent): update config to v3 format

--- nvim/.config/nvim/lua/akriese/plugins/lazy.lua
+++ nvim/.config/nvim/lua/akriese/plugins/lazy.lua
@@ -181,12 +181,9 @@ local plugins = {
     -- Indentation marker
     {
         "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
         config = function()
-            require("indent_blankline").setup({
-                -- for example, context is off by default, use this to turn it on
-                show_current_context = true,
-                show_current_context_start = false,
-            })
+            require("ibl").setup()
         end,
     },
 
