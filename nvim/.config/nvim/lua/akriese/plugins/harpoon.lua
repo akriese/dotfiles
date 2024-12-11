85a2a00a97875a1624a6e6769e39e171db9065b1
Anton Kriese <anton.kriese@gmail.com>
2024-12-11 10:16:33 +0100

    nvim: more harpoon shortcuts

--- nvim/.config/nvim/lua/akriese/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/akriese/plugins/harpoon.lua
@@ -9,5 +9,12 @@ require("harpoon").setup({
     },
 })
 
-F.map("n", "<leader>ha", mark.add_file)
-F.map("n", "<leader>ht", ui.toggle_quick_menu)
+F.map("n", "<leader>ha", mark.add_file, { desc = "Add current file" })
+F.map("n", "<leader>ht", ui.toggle_quick_menu, { desc = "Toggle menu" })
+F.map("n", "<leader>hh", ui.nav_prev, { desc = "Navigate to previous" })
+F.map("n", "<leader>hl", ui.nav_next, { desc = "Navigate to next" })
+for i = 0, 9, 1 do
+    F.map("n", "<leader>h" .. i, function()
+        ui.nav_file(i)
+    end, { desc = "Navigate to file " .. i })
+end
